b2e61997d036e025a0e6025c36550da4099f2ae1
Ashraf <mansooriashraf354@gmail.com>
2026-04-25 16:55:08 +0530

    Fix hardcoded localhost API URL for deployment

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ 
-    baseUrl: 'http://localhost:5000',
+    baseUrl: '',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.userInfo?.token;
       if (token) {
